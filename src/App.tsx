1faa06b43463b748f09c3ae1d8a5f5a4076e8728
nicholas-myers <korefucius00@gmail.com>
2024-06-19 20:17:58 -0700

    add react router dom and three links

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink to="extract" className={({ isActive }) =>
+          isActive ? "active-link" : ""
+        }>
+          Extract
+        </NavLink>
+        <NavLink to="extract" className={({ isActive }) =>
+          isActive ? "active-link" : ""
+        }>
+          Transfer
+        </NavLink>
+        <NavLink to="extract" className={({ isActive }) =>
+          isActive ? "active-link" : ""
+        }>
+          Load
+        </NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
