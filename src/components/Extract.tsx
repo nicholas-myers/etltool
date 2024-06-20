d2cd762c6ee1ee5c80051c04ec3589bfac289d1b
nicholas-myers <korefucius00@gmail.com>
2024-06-20 07:04:00 -0700

    added some steps

--- src/components/Extract.tsx
+++ src/components/Extract.tsx
@@ -1,7 +1,20 @@
 const Extract = () => {
     return (
         <div>
-            Extract content here.
+            <p>
+                1. Pull data from multiple sources, possibly different data types. XML, JSON, excel, etc.
+            </p>
+            <p>
+                2. Determine which fields are to pull and which ones are the source of truth.
+            </p>
+            <p>
+                3. choose format to transfer data to.
+            </p>
+            <p>
+                4. upload to destination database or CMS.
+            </p>
+
+
         </div>
     )
 }
