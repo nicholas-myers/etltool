b3aacfee71c0f1c78f87111599590d4a3ac7fafe
nicholas-myers <korefucius00@gmail.com>
2024-06-20 09:40:05 -0700

    added code example of different source formats

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import { NavLink, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <div className="instructions">
+        <p>
+          1. Pull data from multiple sources, possibly different data types. XML, JSON, excel, etc.
+        </p>
+        <p>
+          2. Determine which fields will be used and which ones are the source of truth.
+        </p>
+        <p>
+          3. choose format to transfer data to.
+        </p>
+        <p>
+          4. upload to destination database or CMS.
+        </p>
+      </div>
       <nav>
         <NavLink to="extract" className={({ isActive }) =>
           isActive ? "active-link" : ""
--- src/components/Extract.tsx
+++ src/components/Extract.tsx
@@ -1,19 +1,37 @@
+
 const Extract = () => {
+
+    const xmlcode = `
+    <contact>
+        <id>1</id>
+        <fname>Nick</fname>
+        <lname>Myers</lname>
+        <phone>3608313070</phone>
+        <email>korefucius00@gmail.com</email>
+    </contact>`
+
+    const jsoncode = `
+    {
+        "id": 1,
+        "firstname": "Nicholas",
+        "lastname": "Myers",
+        "phoneNumber": "360-831-3070",
+        "email": "nicholas.myers.professional@gmail.com"
+    }`
+
+    const csvcode = `
+    FName,LName,Phone,Email
+    Nick,Myers,13608313070,nmyers@resourcedata.com`
+
     return (
         <div>
-            <p>
-                1. Pull data from multiple sources, possibly different data types. XML, JSON, excel, etc.
-            </p>
-            <p>
-                2. Determine which fields are to pull and which ones are the source of truth.
-            </p>
-            <p>
-                3. choose format to transfer data to.
-            </p>
-            <p>
-                4. upload to destination database or CMS.
-            </p>
+            <h2>Source 1: XML example</h2>
 
+            <textarea className="code-block" value={xmlcode} />
+            <h2>Source 2: JSON Example</h2>
+            <textarea className="code-block" value={jsoncode} />
+            <h2>Source 3: CSV Example</h2>
+            <textarea className="code-block" value={csvcode}/>
 
         </div>
     )
